a7d4e7c373d570b8e48160cf62410ba426317900
Meghana19HT <your-github-htmeghana@gmail.com>
2025-06-06 14:23:01 +0530

    Added nodeHealth.sh to check disk, memory and cpu health

---
+++ nodeHealth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+##########################
+#Author:Meghana
+#Date:06/06/2025
+#
+#This script outputs the node health
+#
+#version:v1
+#########################
+
+set -x #debug mode
+
+df -h
+
+free -g
+
+nproc
+
+
+
